ec3f2f0ea5616ec9fa6d6aa9cc17d9bfc8cb5a34
Mykhailo Vovkanych <76040345+myhailo14@users.noreply.github.com>
2022-11-14 22:57:18 +0200

    added config window

--- front-end/src/components/Config.tsx
+++ front-end/src/components/Config.tsx
@@ -1,12 +1,28 @@
 import { DefaultButton, Dialog, DialogFooter, DialogType, Dropdown, IDropdownOption, PrimaryButton, Stack, TextField } from "@fluentui/react";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
 export interface IConfigProps {
     isHidden: boolean,
     hiddenChanger: Dispatch<SetStateAction<boolean>>,
 }
 
+interface IConfigDto {
+    payDesksNumber: number,
+    cooksNumber: number,
+    pizzasNumber: number,
+    pizzaCreationMinTimeInSec: number,
+    cookWorkingStrategy: string,
+    clientsGenerationStrategy: string
+}
+
 const Config: React.FunctionComponent<IConfigProps> = (props: IConfigProps) => {
+
+    const [payDesksNumber, setPayDesksNumber] = useState<number>(1);
+    const [cooksNumber, setCooksNumber] = useState<number>(1);
+    const [pizzasNumber, setPizzasNumber] = useState<number>(1);
+    const [pizzaCreationMinTimeInSec, setCreationTime] = useState<number>(1);
+    const [cookWorkingStrategy, setCookStrategy] = useState<string>('alone');
+    const [clientsGenerationStrategy, setGenerationStrategy] = useState<string>('default');
 
     const dialogContentProps = {
         type: DialogType.normal,
@@ -17,36 +33,77 @@ const Config: React.FunctionComponent<IConfigProps> = (props: IConfigProps) => {
 
     const handleFooterClick = (sendRequest: boolean) => {
         if (sendRequest) {
-            //reques logic goes here
+            fetch(`http://localhost:8080/config`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                  },
+                body: JSON.stringify({
+                    payDesksNumber : payDesksNumber,
+                    cooksNumber : cooksNumber,
+                    pizzasNumber : pizzasNumber,
+                    pizzaCreationMinTimeInSec : pizzaCreationMinTimeInSec,
+                    cookWorkingStrategy : cookWorkingStrategy,
+                    clientsGenerationStrategy : clientsGenerationStrategy
+                })
+                
+            }).then(response => console.log(response));
         }
         props.hiddenChanger(true);
 
-
     }
 
-    const pizzaStrategiesOptions : IDropdownOption[] = 
-    [
-        {key: 'alone', text: "One cook - fulpizza" },
-        {key: 'part', text: "One cook - on process"}
-    ]
+    const pizzaStrategiesOptions: IDropdownOption[] =
+        [
+            { key: 'alone', text: "One cook - full pizza" },
+            { key: 'part', text: "One cook - one process" }
+        ]
 
     const clientStrategiesOptions = [
-        {key: 'default', text: "Fixed Genertion" },
-        {key: 'random', text: "Random Generation"}
+        { key: 'default', text: "Fixed Genertion" },
+        { key: 'random', text: "Random Generation" }
     ]
+
+    const handleInputChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, value: string | undefined) => {
+        const inputValue = Number.parseInt(value!);
+        switch (event.currentTarget.name) {
+            case "desks":
+                setPayDesksNumber(inputValue);
+                break;
+            case "cooks":
+                setCooksNumber(inputValue);
+                break;
+            case "pizzas":
+                setPizzasNumber(inputValue);
+                break;
+            case "time":
+                setCreationTime(inputValue);
+                break;
+        }
+    }
+
+    const handleCookStrategyChange = ( event: React.FormEvent<HTMLDivElement>, option : IDropdownOption<any> | undefined) => {
+        
+        setCookStrategy(option!.key as string);
+    }
+
+    const handleClientStrategyChange = ( event: React.FormEvent<HTMLDivElement>, option : IDropdownOption<any> | undefined) => {
+    
+        setGenerationStrategy(option!.key as string);
+    }
 
     return (
         <Dialog
             hidden={props.isHidden}
             dialogContentProps={dialogContentProps}>
-            <Stack>
-                
-                <TextField suffix="pcs" label="Pay desks number" type="number"></TextField>
-                <TextField suffix="pcs" label="Cooks Number" type="number"></TextField>
-                <TextField suffix="pcs" label="Minimal pizzas number" type="number"></TextField>
-                <TextField suffix="sec" label="Minimal pizza cooking time" type="number"></TextField>
-                <Dropdown label="Pizza cooking strategy" placeholder="Select strategy" options={pizzaStrategiesOptions}></Dropdown>
-                <Dropdown label="Client generation strategy" placeholder="Select strategy" options={clientStrategiesOptions}></Dropdown>
+            <Stack tokens={{ childrenGap: 10 }}>
+                <TextField name="desks" suffix="pcs" label="Pay desks number" type="number" onChange={handleInputChange}></TextField>
+                <TextField name="cooks" suffix="pcs" label="Cooks Number" type="number" onChange={handleInputChange}></TextField>
+                <TextField name="pizzas" suffix="pcs" label="Minimal pizzas number" type="number" onChange={handleInputChange}></TextField>
+                <TextField name="time" suffix="sec" label="Minimal pizza cooking time" type="number" onChange={handleInputChange}></TextField>
+                <Dropdown label="Pizza cooking strategy" onChange={handleCookStrategyChange} placeholder="Select strategy" options={pizzaStrategiesOptions}></Dropdown>
+                <Dropdown label="Client generation strategy" onChange={handleClientStrategyChange} placeholder="Select strategy" options={clientStrategiesOptions}></Dropdown>
             </Stack>
             <DialogFooter>
                 <PrimaryButton onClick={() => handleFooterClick(true)}>Save</PrimaryButton>
