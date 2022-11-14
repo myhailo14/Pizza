4d13224882eac34783abd39f79dd09ddd99e0ff4
Mykhailo Vovkanych <76040345+myhailo14@users.noreply.github.com>
2022-11-14 22:56:48 +0200

    added cross origin

--- pizza/pizza/src/main/java/lab/pizza/controller/MainController.java
+++ pizza/pizza/src/main/java/lab/pizza/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequiredArgsConstructor
 public class MainController {
     private final PizzaService pizzaService;
