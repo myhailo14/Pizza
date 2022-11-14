0e0752ddea5f4d68daa9bf98ae1cc3c7dd053308
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-11-14 19:06:55 +0200

    added function for fetching queues data

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import CashDeskSection from "./components/CashDeskSection/CashDeskSection";
 const App = () => {
+  const [queues, setQueues] = useState([]);
+  function getQueues(){
+    let interval = setInterval(async () => {
+      let response = await fetch("http://localhost:8080/queues");
+      if (response.ok) { 
+        let queuesArr = await response.json();
+        setQueues(queuesArr);
+      } else {
+        alert("Error HTTP: " + response.status);
+      }
+    }, 1000)
+  }
   return (
-    <div>I wanna die</div>
+    <>
+      <CashDeskSection queues={queues} />
+      <button type="button" onClick={getQueues} />
+    </>
   );
 }
 
